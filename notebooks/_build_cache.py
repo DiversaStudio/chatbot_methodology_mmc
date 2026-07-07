eec97faadc9f87045ba9b5394b6af90a74aa2deb
Juan Daniel Vasconez Vela <juan.vasconezvela@gmail.com>
2026-07-07 17:42:34 -0500

    Remove throwaway cache-build script

--- notebooks/_build_cache.py
+++
@@ -1,9 +0,0 @@
-import sys; sys.path.insert(0, "src")
-import mmc_data, mmc_nlp
-m = mmc_data.load_messages()
-msgs = list(m["message"])
-print("messages:", len(msgs))
-mmc_nlp.embed_messages(msgs)
-mmc_nlp.zeroshot_tint(msgs)
-mmc_nlp.emotion_label(msgs)
-print("caches built")
